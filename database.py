324528e9a96af87252dd6e78c35bb74e25478cf3
Victoria OfuasiaAtlassian <105800461+Solu-victoria@users.noreply.github.com>
2026-03-11 17:42:33 +0100

    db mini fix

--- database.py
+++ database.py
@@ -2,9 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
